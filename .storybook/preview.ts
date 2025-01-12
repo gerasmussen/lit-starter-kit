823ce6f5bd75eac36eaece2206a4f051d04c9e6e
Burton Smith <burton@breakstuff.io>
2025-01-11 19:57:58 -0500

    add API docs to MDX pages

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,6 +2,7 @@ import { setCustomElementsManifest } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
 import { setWcStorybookHelpersConfig } from 'wc-storybook-helpers';
 import { withActions } from '@storybook/addon-actions/decorator';
+import { setWcDoxConfig } from 'wc-dox/index.js';
 import './code-bubble-setup.js';
 import './styles.css';
 import '../public/html/index.js';
@@ -10,6 +11,33 @@ import type { Preview } from '@storybook/web-components';
 
 setWcStorybookHelpersConfig({ typeRef: 'expandedType' });
 setCustomElementsManifest(customElements);
+// setWcDoxConfig(customElements);
+setWcDoxConfig(customElements, {
+  imports: {
+    langOnPreTag: true,
+    imports: [
+      {
+        label: 'HTML',
+        lang: 'html',
+        importTemplate: (tagName, className) =>
+          `<script type="module" src="https://cdn.jsdelivr.net/npm/my-library/dist/${tagName}/${className}.js"></script>`,
+      },
+      {
+        label: 'NPM',
+        lang: 'js',
+        importTemplate: (tagName, className) =>
+          `import 'my-library/dist/${tagName}/${className}.js';`,
+      },
+      {
+        label: 'React',
+        lang: 'js',
+        importTemplate: tagName =>
+          `import 'my-library/react/${tagName}/index.js';`,
+      },
+    ],
+  },
+});
+
 
 const preview: Preview = {
   parameters: {
